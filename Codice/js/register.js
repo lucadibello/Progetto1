44f8bcd2f6979a2e67ba9345a83cf40e0c36dceb
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-11-01 18:39:55 +0100

    Commenti del codice

--- Codice/js/register.js
+++ Codice/js/register.js
@@ -1,84 +1,181 @@
+//--> La formattazione è stata fatta automaticamente dall mio IDE chiamto "Visual Studio Code"
+
+//Creo un oggeto di tipo validator, il quale servirà per la validazione dei dati
 var checker = new validator();
 
-function addListeners(){
-    //Add on keydown event to all objects
-    $("#first_name").keydown(function(){manage(this,"nome");});
-    $("#last_name").keydown(function(){manage(this,"cognome");});
-    $("#data_nascita").keydown(function(){manage(this,"data");});
-    $("#email").keydown(function(){manage(this,"email");});
-    $("#citta").keydown(function(){manage(this,"citta");});
-    $("#cap").keydown(function(){manage(this,"cap");});
-    $("#via").keydown(function(){manage(this,"via");});
-    $("#numero_civico").keydown(function(){manage(this,"civico");});
-    //$("#professione").keydown(function(){manage(this,"work");});
-    //$("#hobby").keydown(function(){manage(this,"hobby");});
-    $("#phone").keydown(function(){manage(this,"phone");});
+/**
+ * Questo metodo si occupa di aggiungere gli eventi keyDown e keyUp su tutti gli input del form, questo per avere
+ * una validazione di essi in tempo reale.
+ * Oltre a questo aggiunge la funzione di cancellazione dei dati al bottone 'cancella'.
+ */
+function addListeners() {
+    //Aggiunge evento keydown ai campi
+    $("#first_name").keydown(function () {
+        manage(this, "nome");
+    });
+    $("#last_name").keydown(function () {
+        manage(this, "cognome");
+    });
+    $("#data_nascita").keydown(function () {
+        manage(this, "data");
+    });
+    $("#email").keydown(function () {
+        manage(this, "email");
+    });
+    $("#citta").keydown(function () {
+        manage(this, "citta");
+    });
+    $("#cap").keydown(function () {
+        manage(this, "cap");
+    });
+    $("#via").keydown(function () {
+        manage(this, "via");
+    });
+    $("#numero_civico").keydown(function () {
+        manage(this, "civico");
+    });
+    $("#professione").keydown(function () {
+        manage(this, "work");
+    });
+    $("#hobby").keydown(function () {
+        manage(this, "hobby");
+    });
+    $("#phone").keydown(function () {
+        manage(this, "phone");
+    });
 
 
-    //Add on keyup event for dinamicity
-    $("#first_name").keyup(function(){manage(this,"nome");});
-    $("#last_name").keyup(function(){manage(this,"cognome");});
-    $("#data_nascita").keyup(function(){manage(this,"data");});
-    $("#email").keyup(function(){manage(this,"email");});
-    $("#citta").keyup(function(){manage(this,"citta");});
-    $("#cap").keyup(function(){manage(this,"cap");});
-    $("#via").keyup(function(){manage(this,"via");});
-    $("#numero_civico").keyup(function(){manage(this,"civico");});
-    //$("#professione").keyup(function(){manage(this,"work");});
-    //$("#hobby").keyup(function(){manage(this,"hobby");});
-    $("#phone").keyup(function(){manage(this,"phone");});
+    //Aggiunge evento keyup ai campi
+    $("#first_name").keyup(function () {
+        manage(this, "nome");
+    });
+    $("#last_name").keyup(function () {
+        manage(this, "cognome");
+    });
+    $("#data_nascita").keyup(function () {
+        manage(this, "data");
+    });
+    $("#email").keyup(function () {
+        manage(this, "email");
+    });
+    $("#citta").keyup(function () {
+        manage(this, "citta");
+    });
+    $("#cap").keyup(function () {
+        manage(this, "cap");
+    });
+    $("#via").keyup(function () {
+        manage(this, "via");
+    });
+    $("#numero_civico").keyup(function () {
+        manage(this, "civico");
+    });
+    $("#professione").keyup(function () {
+        manage(this, "work");
+    });
+    $("#hobby").keyup(function () {
+        manage(this, "hobby");
+    });
+    $("#phone").keyup(function () {
+        manage(this, "phone");
+    });
+
+
+    //Questa parte di codice scritta sotto serve per far funzionare il pulsante della cancellazione dei campi
+    $("#cancellaButton").click(function () {
+        //Prende tutti gli input contenuti nel form
+        var $inputs = $('#registerForm :input');
+        var values = {};
+
+        //Cicla ogni input del form 
+        $inputs.each(function () {
+            //Imposta il loro valore (testo) a "" (ovvero nessun testo, saranno vuoti)
+            values[this.name] = $(this).val("");
+
+            //Imposta i colori di default
+            values[this.name].css("borderBottom", "1px solid #9e9e9e");
+            values[this.name].css("boxShadow", "none");
+        });
+        //Fa un update di tutti i campi per resettare le animazioni
+        M.updateTextFields()
+
+        console.log("[INFO] Clean done!")
+    });
 }
 
-function checkAnyError(){
+/**
+ * Questo metodo fa visualizzare un messaggio a schermo che specifica quale campo contiene un valore non valido.
+ * Fa questo appoggiandosi al framework che utilizzo (Materialize) e  al metodo 'manage' il quale 
+ * ritorna lo 'stato di validità del dato.
+ */
+function checkAnyError() {
     var changed = false;
 
-    if(manage(document.getElementById('first_name'),"nome") == false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo nome</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('first_name'), "nome") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo nome</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('last_name'),"cognome")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo cognome</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('last_name'), "cognome") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo cognome</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('data_nascita'),"data")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo Data</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('data_nascita'), "data") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo Data</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('email'),"email")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo Email</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('email'), "email") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo Email</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('citta'),"citta")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo Città</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('citta'), "citta") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo Città</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('cap'),"cap")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo CAP</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('cap'), "cap") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo CAP</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('via'),"via")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo Via</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('via'), "via") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo Via</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('numero_civico'),"civico")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo Numero civico</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('numero_civico'), "civico") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;margin-right:10px;">Errore: </span><span> Campo Numero civico</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    if(manage(document.getElementById('phone'),"phone")== false){
-        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span>Dato non valido nel campo Telefono</span>';
-        M.toast({html: toastHTML});
+    if (manage(document.getElementById('phone'), "phone") == false) {
+        var toastHTML = '<span style="color:red;font-weight:bold;">Errore: </span><span> Campo Telefono</span>';
+        M.toast({
+            html: toastHTML
+        });
         changed = true;
     }
-    
-    if(changed) return false;
+
+    if (changed) return false;
 
     return true;
 }
@@ -89,64 +186,57 @@ const global_length_max = 50;
 const work_length_max = 500;
 const hobby_length_max = 500;
 
-function manage(obj,selector){
+function manage(obj, selector) {
     var status = false;
 
-    //CAMBIA IL COLORE A SECONDA DI COSA RITORNA IL CHECKER
-    if(selector == "nome"){
-        status = checker.nome(obj.value,global_length_min,global_length_max);
-    }
-    else if(selector == "cognome"){
-        status = checker.cognome(obj.value,global_length_min,global_length_max);
-    }
-    else if(selector == "data"){
+    if (selector == "data") {
         status = checker.dataNascita(obj.value);
+    } else {
+        //Rimuove caratteri che potrebbero essere dannosi per il sistema
+
+        //Rimuovo tutti i doppi spazi con uno spazio singolo (questo in tempo reale grazie al metodo 'addListeners()')
+        obj.value = obj.value.replace(/\s\s+/i, ' ');
+
+        /*
+         * Rimuovo il carattere ';'. Questo perchè esso è il separatore utilizzato nel file CSV, se 
+         * utilizzato quando si andrebbe a leggere il file csv si avrebbero diversi problemi.
+         */
+        obj.value = obj.value.replace(/[;]+/i, '');
+
+        //Cambia il colore a seconda di cosa ritorna il validator
+        if (selector == "nome") {
+            status = checker.nome(obj.value, global_length_min, global_length_max);
+        } else if (selector == "cognome") {
+            status = checker.cognome(obj.value, global_length_min, global_length_max);
+        } else if (selector == "cap") {
+            status = checker.cap(obj.value);
+        } else if (selector == "citta") {
+            status = checker.citta(obj.value)
+        } else if (selector == "email") {
+            status = checker.email(obj.value)
+        } else if (selector == "hobby") {
+            status = checker.hobby(obj.value, global_length_min, hobby_length_max);
+        } else if (selector == "civico" || selector == "numeroCivico") {
+            status = checker.numeroCivico(obj.value);
+        } else if (selector == "work") {
+            status = checker.work(obj.value, global_length_min, work_length_max);
+        } else if (selector == "via") {
+            status = checker.via(obj.value, global_length_min, global_length_max);
+        } else if (selector == "phone") {
+            status = checker.phone(obj.value);
+        }
     }
-    else if(selector == "cap"){
-        status = checker.cap(obj.value);
-    }
-    else if(selector == "citta"){
-        status = checker.citta(obj.value)
-    }
-    else if(selector == "email"){
-        status = checker.email(obj.value)
-    }
-    else if(selector == "hobby"){
-        status = checker.hobby(obj.value,global_length_min,hobby_length_max);
-    }
-    else if(selector == "civico" || selector == "numeroCivico"){
-        status = checker.numeroCivico(obj.value);
-    }
-    else if(selector == "work"){
-        status = checker.work(obj.value,global_length_min,work_length_max);
-    }
-    else if(selector == "via"){
-        status = checker.via(obj.value,global_length_min,global_length_max);
-    }
-    else if(selector == "phone"){
-        status = checker.phone(obj.value);
-    }
-    else{
-        alert("Wrong selector on " + obj);
-    }
-    if(status){
+
+    if (status) {
+        //Se il dato è valido (Verde)
         obj.style.borderBottom = "1px solid #4CAF50";
         obj.style.boxShadow = "0 1px 0 0 #4CAF50";
-    }
-    else{
+    } else {
+        //Se il dato non è valido (Rosso)
         obj.style.borderBottom = "1px solid #FF0000";
         obj.style.boxShadow = "0 1px 0 0 #FF0000";
     }
     return status;
 }
 
-function counter(obj){
-}
-
-
-
 console.log("[INFO] Loaded register.js for visual local validation");
-
-
-
-
--- Codice/pages/controllo.php
+++ Codice/pages/controllo.php
@@ -1,19 +1,30 @@
 <?php
+    //Questa pagina utilizza un 'require' perchè i metodi di 'validator.php' sono necessari per il corretto funzionamento della pagina
     require_once("../php/validator.php");
-    //CONTROLLA LA VALIDITÀ DEI DATI, SE NON SONO VALIDI RIMANDA A 'register.php'
+
+    /*
+    *   Questa pagina prima controlla la validità dei dati inviati tramite i metodi di 'validator.php', se essi non sono validi
+    *   viene eseguito un redirect verso la pagina 'register.php'.
+    */
+
     $nome=$cognome=$data_nascita=$sesso=$email=$citta=$cap=$via=$numero_civico=$numero_telefono=$work=$hobby = "null";
 
-    //VALIDATION FLAGS
+    //Flag utili per controllare la validità dei dati e della richiesta
     $data_valid = false;
     $request_valid = false;
     
-    //DATA VALIDATION WRONG FIELDS
+    //Array contenente i nomi dei campi contenenti dati non validi
     $errors = array();
 
+    /*
+    *   Se viene effettuata una richiesta di tipo "POST" controllo se tutti i dati sono presenti tramite il metodo "allSet()", se tutti i dati hanno un valore
+    *   li inserisco in delle variabili, ma prima di questo eseguo un 'clean' del dato tramite il metodo "test_input()", il quale si occupa di evitare un
+    *   cross-site scripting (XSS), quindi il passare tramite dato del codice client-side (ovvero codice JavaScript).
+    */
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         if(allSet()){
             $request_valid = true;
-
+            
             $nome = test_input($_POST["first_name"]);
             $cognome = test_input($_POST["last_name"]);
             $data_nascita = test_input($_POST["data_nascita"]);
@@ -24,10 +35,17 @@
             $via = test_input($_POST["via"]);
             $numero_civico = test_input($_POST["numero_civico"]);
             $numero_telefono = test_input($_POST["phone"]);
-            $work = test_input($_POST["work"]);
-            $hobby = test_input($_POST["hobby"]);
+            $work = isset($_POST["work"]) ? test_input($_POST["work"]) : "";
+            $hobby = isset($_POST["hobby"]) ? test_input($_POST["hobby"]) : "";
+            $len = strlen($work);
 
+            //Valido i dati
             $result = validate_all();
+            
+            /*
+            *   Se i dati sono validi imposto il flag 'data_valid' a 'true' ed eseguo il metodo create_restore_session(), il quale si occupa di tenere in memoria
+            *   tutti i dati della registrazione per essere poi utilizzati nelle altre pagine.
+            */   
             if(is_array($result)){
                 $data_valid = false;
             }
@@ -48,18 +66,27 @@
         return $data;
     }
 
+    /*
+     *  Questo metodo si occupa di controllare se i dati inviati dal form precedente sono impostati. Serve per evitare degli errori se il server
+     *  riceve una richiesta di tipo 'Post' falsa (per esempio fatta tramite 'Burp Suite').
+     */
     function allSet(){
         global $_POST;
         if(isset($_POST["first_name"]) && isset($_POST["last_name"]) && isset($_POST["data_nascita"]) && isset($_POST["sesso"])
         && isset($_POST["email"]) && isset($_POST["citta"]) && isset($_POST["cap"]) && isset($_POST["via"]) && isset($_POST["numero_civico"])
-        && isset($_POST["phone"]) && isset($_POST["work"]) && isset($_POST["hobby"])){
+        && isset($_POST["phone"])){
             return true;
         }
         return false;
     }
 
+    /**
+     * Questo metodo si occupa della validazione dei dati utilizzati nella registrazione. La validazione avviene appoggiandosi
+     * ai metodi presenti nel file 'validator.php'.
+     * @return boolean|array Ritorna 'true' se tutti i dati sono validi, altrimenti ritorna un array contenente tutti i nomi
+     * dei campi contenenti i dati non validi.
+     */
     function validate_all(){
-        //CONTROLLARE QUALE VALIDATORE NON FUNZIONA
         global $nome,$cognome,$data_nascita,$sesso,$email,$citta,$cap,$via,$numero_civico,$numero_telefono,$work,$hobby;
         $errors = array();
 
@@ -82,19 +109,19 @@
             $errors[] = "citta";
         }
         if(!validate_cap($cap)){
-            $errors[] ="cap";
+            $errors[] = "cap";
         }
         if(!validate_phone($numero_telefono)){
-            $errors[] ="phone";
+            $errors[] = "phone";
         }
         if(!validate_numeroCivico($numero_civico)){
-            $errors[] ="civico";
+            $errors[] = "civico";
         }
         if(!validate_work($work)){
-            $errors[] ="work";
+            $errors[] = "work";
         } 
         if(!validate_hobby($hobby)){
-            $errors[] ="hobby";
+            $errors[] = "hobby";
         } 
 
         if(count($errors) == 0){
@@ -105,6 +132,10 @@
         }
     }
 
+    /**
+     * Questo metodo si occupa di tenere in memoria i dati della registrazione per poterli utilizzare nelle pagine successive e precendenti, questo lo fa attraverso
+     * l'array $_SESSION.
+     */
     function create_restore_session(){
         global $nome,$cognome,$data_nascita,$sesso,$email,$citta,$cap,$via,$numero_civico,$numero_telefono,$work,$hobby;
         session_start();
@@ -124,8 +155,17 @@
         $_SESSION["hobby"] = urlencode($hobby);
     }
 
+    /**
+     * Questo metodo si occupa di distruggere la sessione di 'restore'. Esso controlla se il 
+     * flag 'restore' esiste, se c'è distrugge la sessione corrente (distruggendola i dati che 
+     * aveva in memoria vengono persi).
+     * @return boolean Ritorna 'true' se è riuscito a distruggere la sessione, se non è riuscito ritorna 'false'.
+     */
     function close_restore_session(){
-        isset($_SESSION["restore"]) ? session_destroy() : print("not setted");
+        $status = true;
+        isset($_SESSION["restore"]) ? session_destroy() : $status = false;
+
+        return $status;
     }
 ?>
 
@@ -203,12 +243,12 @@
                 </tbody>
             </table>
             <div style="margin-top: 20px;">
-                <form method="post" action="register.php">
+                <form method="post" action="register.php" style="display:inline;">
                     <input type="hidden" name="restore" value="true">
                     <button class="btn waves-effect waves-light teal" style="background-color:rgb(211, 21, 21) !important; margin-right: 10px;">Correggi</td>
                 </form>
                 
-                <form method="post" action="../php/register_user.php">
+                <form method="post" action="../php/register_user.php" style="display:inline;">
                     <input type="hidden" name="register" value="true">
                     <button class="btn waves-effect waves-light teal">Avanti</td>
                 </form>
